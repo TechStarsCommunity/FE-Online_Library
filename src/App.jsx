50e2934cd0d0d2f39528b7a9d8d41092673108a5
Christian Enyia <enyiachristian12@gmail.com>
2023-05-08 08:47:58 +0100

    started the project

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Practice from './Practice'
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>
+        <h1>Tech Stars Community</h1>
+      </div>
+    </>
+  )
+}
+
+export default App;
