9f419f3f3470aed2c3aeb12af56506871dcb4c41
Kennedy Allechenu Ufene <Kennedy.Ufene@abujaelectricity.com>
2023-05-15 21:46:34 +0100

    Project Started

---
+++ src/pages/Signup.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+// change the name of the function to match the name of the file
+const Signup = () => {
+    return (
+        <React.Fragment>
+            {/* Write Your Normal HTml Here */}
+            <div>
+                <h1>My name is Kennedy</h1>
+            </div>
+        </React.Fragment>
+    )
+}
+
+// Also change the name of the function here
+export default Signup;
