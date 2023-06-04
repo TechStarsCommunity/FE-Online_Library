a1f5499ba0419f998e579ce7cf57303c6e43b71b
Enyia Christian <94331754+Christiano112@users.noreply.github.com>
2023-06-04 21:39:44 +0100

    implemented routes and updated the project

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,39 +1,36 @@
-import React from 'react'
-import '../styles/Login.css'
-// change the name of the function to match the name of the file
+import React from "react";
+import "../styles/Login.css";
+
 const Login = () => {
     return (
         <React.Fragment>
-            {/* Write Your Normal HTml Here */}
-            <div className='page'>
-               <div className='one'>
-                <h1>WELCOME!</h1>
+            <div className="page">
+                <div className="one">
+                    <h1>WELCOME!</h1>
+                </div>
+                <div className="two">
+                    <p>Sign in to continue</p>
+                </div>
+                <div className="three">
+                    <label htmlFor="email">Email</label>
+                    <input type="text" placeholder="Type your email" />
+                </div>
+                <div className="four">
+                    <label htmlFor="password">Password</label>
+                    <input type="password" placeholder="8+ characters" />
+                </div>
+                <div>
+                    <button className="login-button">
+                        <span className="login-button-text">Login</span>
+                    </button>
+                </div>
+                <div>
+                    <p>Don{"'"}t have an Account? Sign Up</p>
+                </div>
+                <button>Login as Admin</button>
             </div>
-            <div className='two'>
-                <p>Sign in to continue</p>
-            </div>
-            <div className='three'>
-                <label htmlFor="email">Email</label>
-                <input type="text" placeholder='Type your email' />
-            </div>
-            <div className='four'>
-                <label htmlFor="password">Password</label>
-                <input type="password" placeholder='8+ characters' />
-            </div>
-            <div>
-            <button className='login-button'>
-                <span className='login-button-text'>Login</span>
-            </button>
-            </div>
-            <div>
-               <p>Don't have an Account? Sign Up</p> 
-            </div>
-            <button>Login as Admin</button> 
-            </div>
-            
         </React.Fragment>
-    )
-}
+    );
+};
 
-// Also change the name of the function here
 export default Login;
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,14 +1,88 @@
-import React from 'react'
+import React from "react";
+import "../styles/signup.css";
 
-// change the name of the function to match the name of the file
 const Signup = () => {
     return (
         <React.Fragment>
-            {/* Write Your Normal HTml Here */}
-            
-        </React.Fragment>
-    )
-}
+            <div className="main">
+                <div className="left-section">
+                    <div className="imgg">
+                        <img src="/assets/image.png" className="imge" />
+                        <div className="vector">
+                            <img src="/assets/vector.png" />
+                        </div>
+                    </div>
+                </div>
 
-// Also change the name of the function here
+                <div className="right-section">
+                    <div className="hdngs">
+                        <h2>WELCOME TO BOOKSLAB</h2>
+                        <p>Register your account</p>
+                    </div>
+                    <form action="#" method="get">
+                        <div className="htmlForms">
+                            <label htmlFor="email">
+                                <b>Name</b>
+                            </label>
+                            <input type="text" placeholder="Enter name" name="name" required />
+
+                            <label htmlFor="username">
+                                <b>Username</b>
+                            </label>
+                            <input
+                                type="text"
+                                placeholder="Enter Username"
+                                name="username"
+                                required
+                            />
+
+                            <label htmlFor="email">
+                                <b>Email</b>
+                            </label>
+                            <input type="email" placeholder="Enter Email" name="email" required />
+                        </div>
+
+                        <div className="htmlForms2">
+                            <div className="htmlForms2-00">
+                                <label htmlFor="psw">
+                                    <p>Password</p>{" "}
+                                </label>
+                                <input
+                                    type="password"
+                                    placeholder="Enter Password"
+                                    name="psw"
+                                    required
+                                    minLength="8"
+                                    pattern="[A-Za-z 0-5] {8}"
+                                />
+                            </div>
+                            <div className="htmlForms2-00">
+                                <label htmlFor="psw-repeat">
+                                    <p>Confirm Password </p>{" "}
+                                </label>
+                                <input
+                                    type="password"
+                                    placeholder="Confirm Password"
+                                    name="psw-repeat"
+                                    required
+                                    minLength="8"
+                                    pattern="[A-Za-z 0-5] {8}"
+                                />
+                            </div>
+                        </div>
+                        <div className="btn-sgnup">
+                            <button type="submit" className="btn">
+                                Sign up
+                            </button>
+                            <p>
+                                Already have an account? <a href="#">Login</a>
+                            </p>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </React.Fragment>
+    );
+};
+
 export default Signup;
