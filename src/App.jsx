e5a45e64b2f17fa73ad1fee86acce5be7ae23c20
Enyia Christian <enyiachristian12@gmail.com>
2023-06-17 20:15:13 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,3 @@
-
-import React from "react";
-// import './App.css';
-import Signup from "../src/pages/Signup";
-import Login from "../src/pages/Login";
-import Template from "./pages/Template";
-function App() {
-  return (
-    <React.Fragment>
-      <div>
-        <Login />
-      </div>
-      <div>
-        <Signup />
-
-        <Template />
-      </div>
-    </React.Fragment>
-  );
-}
-
 import React, { lazy } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
