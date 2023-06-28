2cfc3726d3aaeb228028821bd99909cae8413387
Jude-Ugoo <ugooeze0@gmail.com>
2023-06-28 16:40:53 +0100

    uploads page in progress

--- src/components/UploadContents.jsx
+++ src/components/UploadContents.jsx
@@ -36,6 +36,26 @@ const UploadContents = () => {
                             Uploaded by: <span>Oluwaben Akeem</span>
                         </h6>
                     </div>
+                    <div className="uploadContents__contents-content-images">
+                        <div className="uploadContents__contents-content-image">
+                            <img src={Image} alt="uploads img" />
+                            accept
+                        </div>
+                        <div className="uploadContents__contents-content-image">
+                            <img src={Image} alt="uploads img" />
+                            reject
+                        </div>
+                    </div>
+                </div>
+
+                <div className="uploadContents__contents-content">
+                    <img src={Image} alt="uploads img" />
+                    <div className="uploadContents__contents-content-details">
+                        <h4>Starting a successful Career in Tech</h4>
+                        <h6>
+                            Uploaded by: <span>Oluwaben Akeem</span>
+                        </h6>
+                    </div>
 
                     <div className="uploadContents__contents-content-images">
                         <div className="uploadContents__contents-content-image">
