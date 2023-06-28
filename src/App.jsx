20be470dcd61fb3142afd9d5233bc92a98005d5d
canhamzacode <abdulmuizzayo6@gmail.com>
2023-06-29 01:05:05 +0200

    search functionality added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { lazy } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./styles/App.css";
+import Search from "./pages/Search";
 
 const Login = lazy(() => import("./pages/Login"));
 const Signup = lazy(() => import("./pages/Signup"));
@@ -41,6 +42,14 @@ function App() {
                         element={
                             <React.Suspense fallback={<div>Loading...</div>}>
                                 <Contact />
+                            </React.Suspense>
+                        }
+                    />
+                    <Route
+                        path="/search"
+                        element={
+                            <React.Suspense fallback={<div>Loading...</div>}>
+                                <Search />
                             </React.Suspense>
                         }
                     />
