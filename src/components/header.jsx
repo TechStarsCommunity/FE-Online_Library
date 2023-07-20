2134b362a8c1bf063debcd2cb7665a8343608103
Temitope Akinsunmade <temitopeakinsunmade08@gmail.com>
2023-07-20 23:34:59 +0100

    ADD: Added new component 'header.jsx'

---
+++ src/components/header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+// change the name of the function to match the name of the file
+const Header = () => {
+    return (
+        <React.Fragment>
+            <header>
+                
+            </header>
+        </React.Fragment>
+    );
+};
+
+// Also change the name of the function here
+export default Header;
