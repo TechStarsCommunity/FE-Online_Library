77d3f372840e4e47902a8fe7c5f5486b91571612
gabriel ogbu <gabrielogbu28@gmail.com>
2023-07-22 00:00:16 +0100

    added css style to the header html

---
+++ src/components/header.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+// change the name of the function to match the name of the file
+const Header = () => {
+    return (
+        <React.Fragment>
+            <header>
+            <div className="header-logo-parent">
+                <img src={"../assets/booksLab_logo"} alt="" className="header-logo" />
+                </div>
+                <div className="header-links-parent">
+                    <ul>
+                        <li>Home</li>
+                        <li>About Us</li>
+                        <li>Contact US</li>
+                    </ul>
+                </div>
+                <button className="">Get Started</button>
+                <button className="">Login</button>
+                </header>
+        </React.Fragment>
+    );
+};
+
+// Also change the name of the function here
+export default Header;
