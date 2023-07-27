e6c16c8024eb7ebddd21feb98050b3c5915e7f74
Temitope Akinsunmade <temitopeakinsunmade08@gmail.com>
2023-07-27 21:06:55 +0100

    REFACTOR: updated my styling to use tailwind css

--- src/components/button/button.jsx
+++ src/components/button/button.jsx
@@ -3,7 +3,9 @@ import '../../styles/button.css'
 
 const Button = (props) => {
   return(
-    <button className="btn btn-primary">{props.children}</button>
+    <button className="btn btn-primary">
+      {props.children}
+      </button>
 
   )
 }
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -6,14 +6,13 @@ import logo from "../assets/booksLab_logo.png";
 const Header = () => {
     return (
         <React.Fragment>
-            <header>
-                <div className="header-logo-parent">
-
+            <header className="flex flex-row items-center justify-between px-5 py-2.5 bg-white w-full z-[999]">
+                <div className="header-logo-parent ">
                     <img src={"../assets/booksLab_logo"} alt="" className="header-logo" />
-                    <img src={logo} alt="" className="header-logo" />
+                    <img src={logo} alt="" className="max-w-[150px]" />
                 </div>
-                <div className="header-links-parent">
-                    <ul>
+                <div className=" flex flex-row items-center justify-between w-1/2 header-links-parent">
+                    <ul className="flex flex-row gap-2.5">
                         <li>
                             <a href="#" className="active">
                                 Home
@@ -26,15 +25,14 @@ const Header = () => {
                             <a href="#">Contact Us</a>
                         </li>
                     </ul>
-
-                    <div className="btn-parent">
-                        <Button>Get Started</Button> 
-                        <button className="login-btn">Login</button>
+                          
+                    <div className="inline-flex flex-row gap-2.5">
+                        <Button style={{marginTop:"5rem"}}>Get Started</Button>
+                        <button className=" border-[#10b2f3] border-solid border py-2 px-5 rounded-[50px] outline-none cursor-pointer text-[#10b2f3] bg-white ease-in duration-300 hover:scale-[1.1]">
+                            Login
+                        </button>
                     </div>
                 </div>
-                <button className="">Get Started</button>
-                <button className="">Login</button>
-
             </header>
         </React.Fragment>
     );
