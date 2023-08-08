800e02c6e2720aff468da4142bd9b482ca756a71
canhamzacode <abdulmuizzayo6@gmail.com>
2023-08-08 12:32:02 +0200

    implemented the useSubmit and the config for the schema

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "../styles/login.css";
+import { loginSchema } from "../config/schema";
+import useSubmit from "../hooks/useSubmit";
 
 const Login = () => {
+    const { errors, register, handleSubmit } = useSubmit(loginSchema);
+
+    const onLogin = (data) => {
+        console.log(data);
+    };
+
     return (
         <React.Fragment>
             <div className="page">
@@ -9,14 +17,16 @@ const Login = () => {
                     <div className="left-content">
                         <h1>Welcome!</h1>
                         <p>Sign in to continue</p>
-                        <form>
+                        <form onSubmit={handleSubmit(onLogin)}>
                             <div>
                                 <label htmlFor="email">Email</label>
                                 <input
                                     type="text"
                                     placeholder="Type your email"
                                     autoComplete="email"
+                                    {...register("email")}
                                 />
+                                {errors.email && <p className="text-red-600">{errors.email.message}</p>}
                             </div>
                             <div>
                                 <label htmlFor="password">Password</label>
@@ -24,9 +34,11 @@ const Login = () => {
                                     type="password"
                                     placeholder="8+ characters"
                                     autoComplete="current-password"
+                                    {...register("password")}
                                 />
+                                {errors.password && <p className="text-red-600">{errors.password.message}</p>}
                             </div>
-                            <button>Login</button>
+                            <button type="submit">Login</button>
                         </form>
                         <h5>
                             Don{`'`}t have an Account? <a href="register">Sign Up</a>
