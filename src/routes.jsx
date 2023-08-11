a310003550d7360e95f8cc52bf191d677efdcf2e
cEbuka <kastecebuka@gmail.com>
2023-08-11 07:58:16 +0100

    Added mailto in eamil link

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,7 +1,6 @@
 import React, { lazy } from "react";
 import Loading from "./components/loading";
-import { createBrowserRouter } from "react-router-dom"
-
+import { createBrowserRouter } from "react-router-dom";
 
 const Home = lazy(() => import("./pages/home"));
 const Login = lazy(() => import("./pages/login"));
@@ -11,74 +10,73 @@ const Contact = lazy(() => import("./pages/contact"));
 const About = lazy(() => import("./pages/about"));
 const Search = lazy(() => import("./pages/search"));
 
-
 const BrowserRouter = createBrowserRouter([
     {
         path: "/",
         element: (
             <React.Suspense fallback={<Loading />}>
-              <Home />
+                <Home />
             </React.Suspense>
-          ),
+        ),
         children: [
             {
                 path: "/login",
                 element: (
-                  <React.Suspense fallback={<Loading />}>
-                    <Login />
-                  </React.Suspense>
-                )
+                    <React.Suspense fallback={<Loading />}>
+                        <Login />
+                    </React.Suspense>
+                ),
             },
             {
                 path: "/signup",
                 element: (
-                  <React.Suspense fallback={<Loading />}>
-                    <Signup />
-                  </React.Suspense>
-                )
+                    <React.Suspense fallback={<Loading />}>
+                        <Signup />
+                    </React.Suspense>
+                ),
             },
             {
                 path: "/uploads",
                 element: (
-                  <React.Suspense fallback={<Loading />}>
-                    <Uploads />
-                  </React.Suspense>
-                )
+                    <React.Suspense fallback={<Loading />}>
+                        <Uploads />
+                    </React.Suspense>
+                ),
             },
             {
                 path: "/contact",
                 element: (
-                  <React.Suspense fallback={<Loading />}>
-                    <Contact />
-                  </React.Suspense>
-                )
+                    <React.Suspense fallback={<Loading />}>
+                        <Contact />
+                    </React.Suspense>
+                ),
             },
             {
                 path: "/about",
                 element: (
-                  <React.Suspense fallback={<Loading />}>
-                    <About />
-                  </React.Suspense>
-                )
+                    <React.Suspense fallback={<Loading />}>
+                        <About />
+                    </React.Suspense>
+                ),
             },
             {
                 path: "/search",
                 element: (
-                  <React.Suspense fallback={<Loading />}>
-                    <Search />
-                  </React.Suspense>
-                )
+                    <React.Suspense fallback={<Loading />}>
+                        <Search />
+                    </React.Suspense>
+                ),
             },
             {
                 path: "*",
                 element: (
-                  <React.Suspense fallback={<Loading />}>
-                    <Login />
-                  </React.Suspense>
-                )
+                    <React.Suspense fallback={<Loading />}>
+                        <Login />
+                    </React.Suspense>
+                ),
             },
-        ]
-    }
-])
+        ],
+    },
+]);
 
 export default BrowserRouter;
