315bb98d19d5f94847f68e98ba38b079842a892c
ItzAbraham <atandaabraham70@gmail.com>
2023-08-13 08:46:13 +1100

    otpverification done

--- src/pages/otpverification.jsx
+++ src/pages/otpverification.jsx
@@ -9,7 +9,6 @@ import useSubmit from "../hooks/useSubmit";
 import Input from "../components/input/input";
 import { Link } from "react-router-dom";
 
-
 const OTPVerification = () => {
     const { errors, register, handleSubmit } = useSubmit(signUpSchema);
     const dispatch = useAppDispatch();
@@ -30,40 +29,82 @@ const OTPVerification = () => {
                     </div>
                 </div>
                 <div>
-                <form onSubmit={handleSubmit(onSubmit)} className="right-section"></form>
+                    <form onSubmit={handleSubmit(onSubmit)} className="right-section"></form>
                     <div className="container">
-                    <div className="hdngs">
-                        <h2>Verification</h2>
-                        <p>Enter the Code sent to your email below</p>
+                        <div className="hdngs">
+                            <h2>Verification</h2>
+                            <p>Enter the Code sent to your email below</p>
                         </div>
-                        
+
                         <div className="container">
-                        <h2 className="hhd">Code</h2>
-                        <div className="code.container">
-                            <input type="number" className="code" placeholder="0" min="0" max="9" required />
-                             <input type="number" className="code" placeholder="0" min="0" max="9" required />
-                              <input type="number" className="code" placeholder="0" min="0" max="9" required />
-                             <input type="number" className="code" placeholder="0" min="0" max="9" required />
-                             <input type="number" className="code" placeholder="0" min="0" max="9" required />
-                             <input type="number" className="code" placeholder="0" min="0" max="9" required />
-                         </div>
-                         </div>
-                            <p className="hdgn">Didn't receive a code? <a href="resend">Resend</a>
-                            </p> 
-                            
-                            <Link to="/">
-                        <Button style={{width:'50%'}} borderVariant="8" variant="primary">
-                            Continue
-                        </Button>{" "}
-                             </Link>
-                            <p className="hdgn">wrong email? <a href="reset">Reset</a>
-                            </p>
-                             
-                           </div> 
-                           </div>
-                    
+                            <h2 className="hhd">Code</h2>
+                            <div className="code.container">
+                                <input
+                                    type="number"
+                                    className="code"
+                                    placeholder="0"
+                                    min="0"
+                                    max="9"
+                                    required
+                                />
+                                <input
+                                    type="number"
+                                    className="code"
+                                    placeholder="0"
+                                    min="0"
+                                    max="9"
+                                    required
+                                />
+                                <input
+                                    type="number"
+                                    className="code"
+                                    placeholder="0"
+                                    min="0"
+                                    max="9"
+                                    required
+                                />
+                                <input
+                                    type="number"
+                                    className="code"
+                                    placeholder="0"
+                                    min="0"
+                                    max="9"
+                                    required
+                                />
+                                <input
+                                    type="number"
+                                    className="code"
+                                    placeholder="0"
+                                    min="0"
+                                    max="9"
+                                    required
+                                />
+                                <input
+                                    type="number"
+                                    className="code"
+                                    placeholder="0"
+                                    min="0"
+                                    max="9"
+                                    required
+                                />
+                            </div>
+                        </div>
+                        <p className="hdgn">
+                            Didn't receive a code? <a href="resend">Resend</a>
+                        </p>
+
+                        <Link to="/">
+                            <Button style={{ width: "50%" }} borderVariant="8" variant="primary">
+                                Continue
+                            </Button>{" "}
+                        </Link>
+                        <p className="hdgn">
+                            wrong email? <a href="reset">Reset</a>
+                        </p>
+                    </div>
                 </div>
-            </React.Fragment>
+            </div>
+        </React.Fragment>
     );
 };
 
