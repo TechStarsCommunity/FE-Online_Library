f840eeb914a12e02182d9ffe49b8bf37cbd52085
cEbuka <kastecebuka@gmail.com>
2023-08-12 08:42:31 +0100

    used the button.jsx in develop branch

--- src/components/button/button.jsx
+++ src/components/button/button.jsx
@@ -1,8 +1,18 @@
 import React from "react";
 import "../../styles/button.css";
 
-const Button = ({ children }) => {
-    return <button className="btn btn-primary">{children}</button>;
+const Button = ({ children, variant, borderVariant }) => {
+    let buttonClass = "btn"; // Default class
+
+    if (variant === "primary") {
+        buttonClass += " btn-primary";
+    } else if (variant === "secondary") {
+        buttonClass += " btn-secondary ";
+    }
+    if (borderVariant === "noRadius") {
+        buttonClass += " btn-radius ";
+    }
+    return <button className={buttonClass}>{children}</button>;
 };
 
 export default Button;
