8a4471e5f2436a80efb33b0edd0ae05d2658a17e
blessingsmart <bublessingsmart@gmail.com>
2023-08-13 20:15:13 +0100

    new file:   src/assets/loginImage.svg 	modified:   src/pages/login.jsx 	modified:   src/styles/login.css

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,20 +1,22 @@
 import React from "react";
 import "../styles/login.css";
+import loginImage from "../assets/loginImage.svg";
+import Logo from "../assets/booksLab_logo.png";
 
 const Login = () => {
     return (
         <React.Fragment>
-            <div className="page">
+            <div className=" bg-white grid grid-cols-[repeat(auto-fit,minmax(200px,1fr))] gap-0 h-screen w-full m-0 p-0">
                 <div className="left">
-                    <div className="left-content">
-                        <h1>Welcome!</h1>
-                        <p>Sign in to continue</p>
+                    <div className="left-content py-20">
+                        <h1 className="text-sky-950 text-4xl font-semibold">Welcome Back</h1>
+                        <p className="text-black text-base font-normal">Sign in to continue</p>
                         <form>
                             <div>
                                 <label htmlFor="email">Email</label>
                                 <input
                                     type="text"
-                                    placeholder="Type your email"
+                                    placeholder="tola001@gmail.com"
                                     autoComplete="email"
                                 />
                             </div>
@@ -26,16 +28,16 @@ const Login = () => {
                                     autoComplete="current-password"
                                 />
                             </div>
-                            <button>Login</button>
+                            <button className="bg-gradient-to-r from-cyan-600 to-teal-400 text-white">Login</button>
                         </form>
-                        <h5>
-                            Don{`'`}t have an Account? <a href="register">Sign Up</a>
+                        <h5 className="text-2xl font-medium">
+                            Don{`'`}t have an Account? <a className="text-sky-950 text-2xl font-bold" href="register">Sign Up</a>
                         </h5>
-                        <button>Login as Admin</button>
+                        <button className="bg-gradient-to-r from-cyan-600 to-teal-400 text-white">Login as Admin</button>
                     </div>
                 </div>
                 <div className="right">
-                    <img src={"../assets/image_icon.png"} alt="image" />
+                    <img src={loginImage} alt="image" />
                 </div>
             </div>
         </React.Fragment>
