db074ef897cf95e8d6708675147f75e575a2dca7
blessingsmart <bublessingsmart@gmail.com>
2023-08-13 18:46:37 +0100

    modified:   src/components/roadmap.jsx 	new file:   src/components/roadmapicons.jsx

--- src/components/roadmap.jsx
+++ src/components/roadmap.jsx
@@ -1,98 +1,53 @@
-import React, {useState} from 'react'
-import frontendIcon from "../assets/frontendicon.svg"
-import backendIcon from "../assets/backendicon.svg"
-import designIcon from "../assets/designicon.svg"
-import writerIcon from "../assets/writericon.svg"
-import dataanalysisIcon from "../assets/dataanalysisicon.svg"
-import frontendIcon2 from "../assets/frontendicon2.svg"
-import backendIcon2 from "../assets/backendicon2.svg"
-import designIcon2 from "../assets/designicon2.svg"
-import writerIcon2 from "../assets/writericon2.svg"
-import dataanalysisIcon2 from "../assets/dataanalysisicon2.svg"
+import React, { useState } from 'react';
+import { iconData, iconData2 } from './roadmapicons';
 
-
-const Roadmap = () => {
-  const [icons, setIcons] = useState([
-    {
-      id: 1,
-      src: frontendIcon,
-      show: false,
-    },
-    {
-      id: 2,
-      src: backendIcon,
-      show: false,
-    },
-    {
-      id: 3,
-      src: designIcon,
-      show: false,
-    },
-    {
-      id: 4,
-      src: writerIcon,
-      show: false,
-    },
-    {
-      id: 5,
-      src: dataanalysisIcon,
-      show: false,
-    }
-  ]);
-  const icons2 = [
-    {
-      id: 1,
-      src: frontendIcon2,
-    },
-    {
-      id: 2,
-      src: backendIcon2,
-    },
-    {
-      id: 3,
-      src: designIcon2,
-    },
-    {
-      id: 4,
-      src: writerIcon2,
-    },
-    {
-      id: 5,
-      src: dataanalysisIcon2,
-    }
-  ]
+const roadmap = () => {
+  const [icons, setIcons] = useState(iconData);
 
   const toggleIcon2 = (id) => {
     setIcons((prevIcons) =>
-    prevIcons.map((icon) => {
+      prevIcons.map((icon) => {
         if (icon.id === id) {
-            return { ...icon, show: !icon.show};
+          return { ...icon, show: !icon.show };
         }
-        return icon;
-    })
+        return { ...icon, show: false };
+      })
     );
   };
 
   return (
-      <React.Fragment>
-          <div className='w-full bg-emerald-50 p-12'>
-              <div className='left-0 top-0 text-sky-950 text-5xl font-bold leading-normal'>
-                        Roadmap
-              </div>
-              <div className='flex mt-20 overflow-x-auto overflow-y-hidden gap-20 lg:gap-40'>
-                        {icons.map (({id,src, show }) => (
-                  <div key={id} className='flex-shrink-0' onClick={() => toggleIcon2(id)}>
-                          {show ?(
-                    <img src={icons2[id -1].src} alt="career path" />
-                          ):(
-                    <img src={src} alt="career path1" />
-                          )}
+    <React.Fragment>
+      <div className='bg-[#E9FCF4] w-full p-12'>
+        <div className='left-0 top-0 text-sky-950 text-5xl font-bold leading-normal'>
+          Roadmap
+        </div>
+        <div className='flex mt-20 overflow-x-auto overflow-y-hidden gap-20 lg:gap-40'>
+          {icons.map(({ id, show }) => (
+            <div key={id} className='flex-shrink-0 flex flex-col items-center ' onClick={() => toggleIcon2(id)}>
+              {show ? (
+                iconData2[id - 1].images.map((image, index) => (
+                  <div key={index} className="text-center">
+                    <a
+                      href={image.url}
+                      target='_blank'
+                      rel='noopener noreferrer'
+                    >
+                      <div className="flex flex-col items-center justify-center">
+                        <img src={image.src} alt={image.title} />
+                        <p className="mt-1 text-sky-950 text-base font-semibold capitalize leading-normal">{image.title}</p>
+                      </div>
+                    </a>
                   </div>
-                    ))}    
-              </div>
-          </div>
-      </React.Fragment>
+                ))
+              ) : (
+                <img src={iconData[id - 1].src} alt='' />
+              )}
+            </div>
+          ))}
+        </div>
+      </div>
+    </React.Fragment>
   );
 };
 
-export default Roadmap;
+export default roadmap;
---
+++ src/components/roadmapicons.jsx
@@ -0,0 +1,255 @@
+import frontendIcon from "../assets/frontendicon.svg";
+import backendIcon from "../assets/backendicon.svg";
+import designIcon from "../assets/designicon.svg";
+import writerIcon from "../assets/writericon.svg";
+import dataanalysisIcon from "../assets/dataanalysisicon.svg";
+import arrow1 from "../assets/Arrow 1.svg";
+import html from "../assets/Html.svg";
+import arrow2 from "../assets/Arrow 2.svg";
+import css1 from "../assets/CSS3.svg";
+import python2 from "../assets/Python.svg";
+import django2 from "../assets/Django.svg";
+import mysql2 from "../assets/Mysql.svg";
+import DesignBasics from "../assets/DesignBasics.svg";
+import uiDesign from "../assets/UIDesign.svg";
+import uxinterface from "../assets/Uxinterface.svg";
+import writingIcon from "../assets/Writing.svg";
+import AdvancedWriting from "../assets/AdvancedWriting.svg";
+import CopyWriting from "../assets/CopyWriting.svg";
+import Analytics from "../assets/Analytics.svg";
+import JavaScript from "../assets/js.svg";
+
+
+
+    const frontendIcon2 = [
+    {
+      id: 1,
+      src: frontendIcon,
+    },
+    {
+      id: 2,
+      src: arrow1,
+    },
+    {
+      id: 3,
+      src: html,
+      title: "HTML",
+      url: "https://example.com/html-link"
+    },
+    {
+      id: 4,
+      src: arrow2,
+    },
+    {
+      id: 5,
+      src: css1,
+      title: "CSS",
+      url: "https://example.com/html-link"
+    },
+    {
+      id: 6,
+      src: arrow2,
+    },
+    {
+      id: 7,
+      src: JavaScript,
+      title: "JavaScript",
+      url: "https://example.com/html-link"
+    },
+  ];
+    const backendIcon2 = [
+    {
+      id: 1,
+      src: backendIcon,
+    },
+    {
+      id: 2,
+      src: arrow1,
+    },
+    {
+      id: 3,
+      src: python2,
+      title: "Python",
+      url: "https://example.com/html-link"
+    },
+    {
+      id: 4,
+      src: arrow2,
+    },
+    {
+      id: 5,
+      src: django2,
+      title: "Django",
+      url: "https://example.com/html-link"
+    },
+    {
+      id: 6,
+      src: arrow2,
+    },
+    {
+      id: 7,
+      src: mysql2,
+      title: "MySQL",
+      url: "https://example.com/html-link"
+    },
+  ];
+    const designIcon2 = [
+    {
+      id: 1,
+      src: designIcon,
+    },
+    {
+      id: 2,
+      src: arrow1,
+    },
+    {
+      id: 3,
+      src: DesignBasics,
+      title: "Design Basics",
+      url: "https://example.com/html-link"
+    },
+    {
+      id: 4,
+      src: arrow2,
+    },
+    {
+      id: 5,
+      src: uiDesign,
+      title: "UI Design",
+      url: "https://example.com/html-link"
+    },
+    {
+      id: 6,
+      src: arrow2,
+    },
+    {
+      id: 7,
+      src: uxinterface,
+      title: "UX Design",
+      url: "https://example.com/html-link"
+    },
+  ];
+    const writerIcon2 = [
+    {
+      id: 1,
+      src: writerIcon,
+    },
+    {
+      id: 2,
+      src: arrow1,
+    },
+    {
+      id: 3,
+      src: writingIcon,
+      title: "Writing Basics",
+      url: "https://example.com/html-link"
+    },
+    {
+      id: 4,
+      src: arrow2,
+    },
+    {
+      id: 5,
+      src: CopyWriting,
+      title: "copywriting",
+      url: "https://example.com/html-link"
+    },
+    {
+      id: 6,
+      src: arrow2,
+    },
+    {
+      id: 7,
+      src: AdvancedWriting,
+      title: "advanced writing",
+      url: "https://example.com/html-link"
+    },
+  ];
+    const dataanalysisIcon2 = [
+    {
+      id: 1,
+      src: dataanalysisIcon,
+    },
+    {
+      id: 2,
+      src: arrow1,
+    },
+    {
+      id: 3,
+      src: Analytics,
+      title: "analysis I",
+      url: "https://example.com/html-link"
+    },
+    {
+      id: 4,
+      src: arrow2,
+    },
+    {
+      id: 5,
+      src: Analytics,
+      title: "analysis II",
+      url: "https://example.com/html-link"
+    },
+    {
+      id: 6,
+      src: arrow2,
+    },
+    {
+      id: 7,
+      src: Analytics,
+      title: "analysis III",
+      url: "https://example.com/html-link"
+    },
+  ];
+
+
+export const iconData = [
+  {
+    id: 1,
+    src: frontendIcon,
+    show: false,
+  },
+  {
+    id: 2,
+    src: backendIcon,
+    show: false,
+  },
+  {
+    id: 3,
+    src: designIcon,
+    show: false,
+  },
+  {
+    id: 4,
+    src: writerIcon,
+    show: false,
+  },
+  {
+    id: 5,
+    src: dataanalysisIcon,
+    show: false,
+  },
+];
+
+export const iconData2 = [
+  {
+    id: 1,
+    images: frontendIcon2,
+  },
+  {
+    id: 2,
+    images: backendIcon2,
+  },
+  {
+    id: 3,
+    images: designIcon2,
+  },
+  {
+    id: 4,
+    images: writerIcon2,
+  },
+  {
+    id: 5,
+    images: dataanalysisIcon2,
+  },
+];
