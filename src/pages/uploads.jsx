0b05dc91a03b98ad7ad6290547a1f7b75e95f0fc
gafaradetunji <gafaradetunji471@gmail.com>
2023-08-27 17:02:37 +0100

    implemented the sideBar component and made sure it is connected to all corresponding components

--- src/pages/uploads.jsx
+++ src/pages/uploads.jsx
@@ -1,16 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/uploads.css";
 import LeftSidebar from "../components/leftSideBar";
-import RightSidebar from "../components/rightSideBar";
-import UploadContents from "../components/uploadContents";
+import { Tabs as Tabs } from "../data/leftSideBarData"
 
 const Uploads = () => {
+    const [active, setActive] = useState(1)
+    const activeTabInfo = Tabs.find((tab) => tab.id === active);
     return (
         <React.Fragment>
-            <div className="uploads">
-                <LeftSidebar />
-                <UploadContents />
-                <RightSidebar />
+            <div className="grid grid-cols-5 gap-4 w-full overflow-x-none">
+              <div className="col-span-2 bg-[#DFF6FF] overflow-y-auto h-screen px-4 py-4 fixed ">
+                <LeftSidebar active={active} setActive={setActive} className=""/>
+              </div>
+              <div className="col-span-3 ml-[400px]">
+                  {activeTabInfo && <activeTabInfo.component/>}
+              </div>
             </div>
         </React.Fragment>
     );
