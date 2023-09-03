7ebc02e037c9bce3ed86ee56fd53cbbc6d26cadf
gafaradetunji <gafaradetunji471@gmail.com>
2023-09-03 15:24:55 +0100

    Little changes on the Left SideBar

--- src/components/leftSideBar.jsx
+++ src/components/leftSideBar.jsx
@@ -3,6 +3,7 @@ import "../styles/leftSideBar.css";
 import booksLabLogo from "/booksLab_logo.png"
 import { Tabs as Tabs } from "../data/leftSideBarData"
 import { AiOutlineLogout } from "react-icons/ai";
+import Button from "./button";
 
 const LeftSidebar = ({ active, setActive }) => {
     const handleClick = (id) => {
@@ -11,10 +12,10 @@ const LeftSidebar = ({ active, setActive }) => {
     
     return (
         <React.Fragment>
-            <div className="">
-                <h1 className="mt-[50px] mb-[50px]"><img src={booksLabLogo} alt="Logo" /></h1>
+            <div className="relative bg-black h-full">
+                <h1 className="mt-[50px] mb-[50px] mx-auto w-[60%]"><img src={booksLabLogo} alt="Logo" /></h1>
 
-                <div className="">
+                <div className="mt-[100px]">
                     {Tabs.map((data) => {
                         const { id, name, icon } = data
                         return(
@@ -22,18 +23,19 @@ const LeftSidebar = ({ active, setActive }) => {
                               key={id}
                               onClick={() => handleClick(id)}
                               className={`mb-[25px] text-[1rem] mx-5 flex text-center text-[#191919] font-serif font-medium
-                              px-8 button cursor-pointer ${active === data.id ? "button-active-style" : ""}`} 
+                              px-8 button cursor-pointer ${active === id ? "button-active-style" : ""}`} 
                               >
-                                <span className="me-4">{icon}</span>
+                                <span className="me-4 text-[24px]">{icon}</span>
                                 <button>{name}</button>
+                                {/* <Button className='text-black'>{name}</Button> */}
                             </div>
                         )
                     })}
                     <div 
-                        className={`mb-[25px] text-[1rem] mx-5 flex text-center text-[#191919] font-serif font-medium
-                        px-8 button cursor-pointer absolute bottom-5`} 
+                        className={`text-[1rem] mx-5 flex text-center text-[#191919] font-serif font-medium
+                        px-8 button cursor-pointer absolute mt-12 flex items-center`} 
                         >
-                        <span className="me-4"> <AiOutlineLogout /></span>
+                        <span className="me-4 text-[24px]"> <AiOutlineLogout /></span>
                         <button>Logout</button>
                     </div>
                 </div>
