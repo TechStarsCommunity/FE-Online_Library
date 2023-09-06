7c42234c89b4d8bda80bc94742822bc449be15ee
Babatunde <68726835+tundeloper@users.noreply.github.com>
2023-09-06 23:20:42 +0100

    input form

--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -2,35 +2,41 @@ import React from 'react'
 import '../styles/profile.css'
 import profileImg from '../../public/bgimage.png'
 import SettingSvg from '../../public/settingSvg'
+import Button from '../components/button'
+
 export default function Profile() {
   return (
     <div className='profile-container'>
-        <div className='left-side'>left</div>
-        <div className='right-side'>
+        <section className='left-side'>left</section>
+        <section className='right-side'>
           <div className='bg-container'>
             <img src={profileImg} alt='bgImage' className='bg-image' />
             
             <p className='text'>Edit your Infomatin</p>
           
             <form className='profile-form'>
-            <div className='input-container'>
+            <div className='input-container-row'>
               <input placeholder='name' type='name' className='input' />
               <input placeholder='Email' type='gmail' className='input' />
               <input placeholder='Phone' type='phone' className='input' />
             </div>
             
-            <div className='input-container contain'>
-              <input placeholder='address' type='address' className='input' />
+            <div className='input-container-colomn'>
+              <input placeholder='address' type='address' className='input' style={{height: '5rem'}} />
               <input placeholder='Email' className='input' />
               <input placeholder='Select level' className='input'  />
             </div>
           </form>
+
+          <div className='btn-container'>
+            <Button variant='primary'>DONE</Button>
+          </div>
         </div>
         
         <div className='setting'>
           <SettingSvg />
         </div>
-        </div>
+      </section>
     </div>
   )
 }
