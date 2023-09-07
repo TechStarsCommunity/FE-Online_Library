5a499fa08bc14476847cc9f5d8b0957cd4cd5e53
canhamzacode <abdulmuizzayo6@gmail.com>
2023-09-07 19:08:40 +0200

    made changes to the data

--- src/components/topCategoryV2.jsx
+++ src/components/topCategoryV2.jsx
@@ -1,36 +1,36 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 import tech from "/tech.svg";
-import Button from './button';
-import { BsArrowRightCircleFill, BsArrowLeftCircleFill } from "react-icons/bs"
-
+import Button from "./button";
+import { BsArrowRightCircleFill, BsArrowLeftCircleFill } from "react-icons/bs";
+import { categories } from "../data/category";
 
 const TopCategoryV2 = () => {
     const [currentIndex, setCurrentIndex] = useState(0);
-    const categories = [
-        {
-            image: "/tech.png",
-            title: "Tech",
-            description: "This refers to the application of scientific knowledge and tools to solve practical problems and improve various aspects of human life. It encompasses a wide range of tools, techniques, systems, and devices that have been developed over time to enhance productivity, communication, convenience, and more. All distributions of tech books.",
-        },
-        {
-            image: "/fruit.png",
-            title: "Nutrition",
-            description: "Boost your Nutrition and Health with these books ",
-        },
-        {
-            image: "/women.png",
-            title: "Sports",
-            description: "For Athletes, get in the zone and aim for the summit.",
-        },
-        {
-            image: "/tech.png",
-            title: "Tech",
-            description: "All distributions of tech books",
-        },
-    ];
+    // const categories = [
+    //     {
+    //         image: "/tech.png",
+    //         title: "Tech",
+    //         description:
+    //             "This refers to the application of scientific knowledge and tools to solve practical problems and improve various aspects of human life. It encompasses a wide range of tools, techniques, systems, and devices that have been developed over time to enhance productivity, communication, convenience, and more. All distributions of tech books.",
+    //     },
+    //     {
+    //         image: "/fruit.png",
+    //         title: "Nutrition",
+    //         description: "Boost your Nutrition and Health with these books ",
+    //     },
+    //     {
+    //         image: "/women.png",
+    //         title: "Sports",
+    //         description: "For Athletes, get in the zone and aim for the summit.",
+    //     },
+    //     {
+    //         image: "/tech.png",
+    //         title: "Tech",
+    //         description: "All distributions of tech books",
+    //     },
+    // ];
 
     const currentCategory = categories[currentIndex];
-
 
     const goToNext = () => {
         if (currentIndex < categories.length - 1) {
@@ -45,35 +45,37 @@ const TopCategoryV2 = () => {
     };
 
     return (
-        <div className='h-[90vh] md:h-[100vh] w-full relative '>
-            <img src={currentCategory.image} alt="" className='w-full h-full object-cover' />
-            <div className='absolute top-0 left-0 w-full md:h-[100vh] h-[90vh] myBg p-[30px] text-white flex flex-col gap-[30px]'>
+        <div className="h-[90vh] md:h-[100vh] w-full relative ">
+            <img src={currentCategory.image} alt="" className="w-full h-full object-cover" />
+            <div className="absolute top-0 left-0 w-full md:h-[100vh] h-[90vh] myBg p-[30px] text-white flex flex-col gap-[30px]">
                 <h2 className="text-[35px] md:text-[50px] font-bold">Top Categories</h2>
-                <div className='grid md:grid-cols-2 grid-cols-1 gap-[40px] items-center justify-center'>
-                    <div className='flex flex-col gap-[30px]'>
-                        <div className='grid gap-[30px]'>
-                            <h4 className='text-[25px] md:text-[35px]'>{currentCategory.title}</h4>
-                            <p className='text-[18px]'>
-                                {currentCategory.description}
-                            </p>
+                <div className="grid md:grid-cols-2 grid-cols-1 gap-[40px] items-center justify-center">
+                    <div className="flex flex-col gap-[30px]">
+                        <div className="grid gap-[30px]">
+                            <h4 className="text-[25px] md:text-[35px]">{currentCategory.title}</h4>
+                            <p className="text-[18px]">{currentCategory.description}</p>
                         </div>
-                        <Button children={"Learn More"} variant="primary" style={{ width: "250px" }} />
+                        <Button
+                            children={"Learn More"}
+                            variant="primary"
+                            style={{ width: "250px" }}
+                        />
                     </div>
-                    <div className='hidden md:flex'>
-                        <img src={currentCategory.image} alt="" className='w-full max-h-[400px]' />
+                    <div className="hidden md:flex">
+                        <img src={currentCategory.image} alt="" className="w-full max-h-[400px]" />
                     </div>
                 </div>
-                <div className='py-[20px] px-[40px] w-full flex items-center justify-between absolute bottom-[20px] left-0 '>
+                <div className="py-[20px] px-[40px] w-full flex items-center justify-between absolute bottom-[20px] left-0 ">
                     <BsArrowLeftCircleFill
                         size={30}
-                        style={{ cursor: currentIndex === 0 ? 'not-allowed' : 'pointer' }}
+                        style={{ cursor: currentIndex === 0 ? "not-allowed" : "pointer" }}
                         onClick={goToPrevious}
                     />
-                    <div className=' w-[50%] rounded-lg flex items-center'>
+                    <div className=" w-[50%] rounded-lg flex items-center">
                         {categories.map((_, index) => (
                             <div
                                 key={index}
-                                className={`flex-1 h-[7px] bg-[#fff] rounded-lg ${index < currentIndex + 1 ? 'bg-[red]' : ''
+                                className={`flex-1 h-[7px] bg-[#fff] rounded-lg ${index < currentIndex + 1 ? "bg-[red]" : ""
                                     }`}
                             ></div>
                         ))}
@@ -81,13 +83,16 @@ const TopCategoryV2 = () => {
 
                     <BsArrowRightCircleFill
                         size={30}
-                        style={{ cursor: currentIndex === categories.length - 1 ? 'not-allowed' : 'pointer' }}
+                        style={{
+                            cursor:
+                                currentIndex === categories.length - 1 ? "not-allowed" : "pointer",
+                        }}
                         onClick={goToNext}
                     />
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default TopCategoryV2
+export default TopCategoryV2;
