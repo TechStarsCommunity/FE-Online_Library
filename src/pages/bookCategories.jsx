7bff00620394f961bad0f7306452804b9545f12e
Enyia Christian <enyiachristian12@gmail.com>
2023-09-07 15:27:06 +0100

    optimized the code

--- src/pages/bookCategories.jsx
+++ src/pages/bookCategories.jsx
@@ -3,68 +3,72 @@ import Button from "../components/button";
 import { Link } from "react-router-dom";
 import CategoriesButton from "../components/categoriesButton";
 
-// change the name of the function to match the name of the file
+const categoryNames = [
+  "Editor's Pick",
+  "Frontend Development",
+  "Product Design",
+  "Academic & Education",
+  "Biography",
+  "Children & Youth",
+  "Fiction & Literature",
+  "Religion",
+  "Lifestyle",
+  "Politics & Laws",
+  "Science & Research",
+  "Economics",
+  "Food",
+  "Marketing",
+  "Parenting",
+  "Relationships",
+  "Backend Development",
+  "Project Management",
+  "Arts & Design",
+  "Business & Career",
+  "Environment",
+  "Health & Fitness",
+  "Personal Growth",
+  "Tech",
+  "Entertainment",
+  "History",
+  "Motivation",
+  "Psychology",
+  "Sports",
+];
+
 const BookCategories = () => {
-    return (
-        <React.Fragment>
-            <body className="p-16">
-                <header>
-                    <div>
-                        <h1 className="font-semibold text-4xl">Book Categories</h1>
-                    </div>
-                    <div className="flex justify-between mt-2">
-                        <p className="text-[#5B5B5B]">Select your favorite Categories</p>
-                        <Link to="/">
-                            {" "}
-                            <p className="text-[#5B5B5B]">Switch to RoadMap</p>
-                        </Link>
-                    </div>
-                </header>
-                <main>
-                    <div className="my-12 flex flex-wrap gap-6  ">
-                        <CategoriesButton initialText="Editor's Pick" />
-                        <CategoriesButton initialText="Frontend Development" />
-                        <CategoriesButton initialText="Product Design" />
-                        <CategoriesButton initialText="Academic & Education" />
-                        <CategoriesButton initialText="Biography" />
-                        <CategoriesButton initialText="Children & Youth" />
-                        <CategoriesButton initialText="Fiction & Literature" />
-                        <CategoriesButton initialText="Religion" />
-                        <CategoriesButton initialText="Lifestyle" />
-                        <CategoriesButton initialText="Politics & Laws" />
-                        <CategoriesButton initialText="Science & Research" />
-                        <CategoriesButton initialText="Economics" />
-                        <CategoriesButton initialText="Food" />
-                        <CategoriesButton initialText="Marketing" />
-                        <CategoriesButton initialText="Parenting" />
-                        <CategoriesButton initialText="Relationships" />
-                        <CategoriesButton initialText="Backend Development" />
-                        <CategoriesButton initialText="Project Management" />
-                        <CategoriesButton initialText="Arts & Design" />
-                        <CategoriesButton initialText="Business & Career" />
-                        <CategoriesButton initialText="Environment" />
-                        <CategoriesButton initialText="Health & Fitness" />
-                        <CategoriesButton initialText="Personal Growth" />
-                        <CategoriesButton initialText="Tech" />
-                        <CategoriesButton initialText="Entertainment" />
-                        <CategoriesButton initialText="History" />
-                        <CategoriesButton initialText="Motivation" />
-                        <CategoriesButton initialText="Psychology" />
-                        <CategoriesButton initialText="Sports" />
-                    </div>
-                </main>
-                <footer className="flex justify-between">
-                    <div className="w-2/5">
-                        <Button borderVariant="noRadius" variant="primary">
-                            Continue
-                        </Button>{" "}
-                    </div>
-                    <p>Skip</p>
-                </footer>
-            </body>
-        </React.Fragment>
-    );
+  return (
+    <React.Fragment>
+      <body className="p-16">
+        <header>
+          <div>
+            <h1 className="font-semibold text-4xl">Book Categories</h1>
+          </div>
+          <div className="flex justify-between mt-2">
+            <p className="text-[#5B5B5B]">Select your favorite Categories</p>
+            <Link to="/">
+              {" "}
+              <p className="text-[#5B5B5B]">Switch to RoadMap</p>
+            </Link>
+          </div>
+        </header>
+        <main>
+          <div className="my-12 flex flex-wrap gap-6">
+            {categoryNames.map((category, index) => (
+              <CategoriesButton key={index} initialText={category} />
+            ))}
+          </div>
+        </main>
+        <footer className="flex justify-between">
+          <div className="w-2/5">
+            <Button borderVariant="noRadius" variant="primary">
+              Continue
+            </Button>{" "}
+          </div>
+          <p>Skip</p>
+        </footer>
+      </body>
+    </React.Fragment>
+  );
 };
 
-// Also change the name of the function here
 export default BookCategories;
