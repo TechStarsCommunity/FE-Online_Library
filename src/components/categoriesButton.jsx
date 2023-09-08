c7e9cf292f0a79c495cb26959fbfed54995384da
Temitope Akinsunmade <temitopeakinsunmade08@gmail.com>
2023-09-08 21:25:11 +0100

    UPDATE bookcategories page

--- src/components/categoriesButton.jsx
+++ src/components/categoriesButton.jsx
@@ -1,44 +1,23 @@
 import React, { useState } from "react";
+import "../styles/categoriesButton.css";
 
 function CategoriesButton({ initialText }) {
-    const [isClicked, setIsClicked] = useState(false);
-    // const [isHovered, setIsHovered] = useState(false);
+  const [isClicked, setIsClicked] = useState(false);
 
-    // const handleMouseEnter = () => {
-    //     setIsHovered(true);
-    // };
+  const handleClick = () => {
+    setIsClicked(!isClicked);
+  };
 
-    // const handleMouseLeave = () => {
-    //     setIsHovered(false);
-    // };
+  const buttonClassName = `button ${isClicked ? "clicked" : ""}`;
 
-    const handleClick = () => {
-        setIsClicked(!isClicked);
-    };
-
-    // Define the class name based on the button's state and hover state
-    const buttonClassName = `text-[#8A8A8A] hover:bg-[#8A8A8A]
-                                                 
-                                                  hover:bg-zinc-400  
-                          border-2 p-2 text-base rounded-[3.75rem]
-                         transition-all duration-300`;
-
-    // Define the style based on the button's click state
-    const buttonStyle = {
-        backgroundColor: isClicked ? "#D1FFF3" : "white",
-    };
-
-    return (
-        <button
-            className={buttonClassName}
-            style={buttonStyle}
-            // onMouseEnter={handleMouseEnter}
-            // onMouseLeave={handleMouseLeave}
-            onClick={handleClick}
-        >
-            {initialText}
-        </button>
-    );
+  return (
+    <button
+      className={buttonClassName}
+      onClick={handleClick}
+    >
+      {initialText}
+    </button>
+  );
 }
 
 export default CategoriesButton;
--- src/pages/bookCategories.jsx
+++ src/pages/bookCategories.jsx
@@ -4,71 +4,71 @@ import { Link } from "react-router-dom";
 import CategoriesButton from "../components/categoriesButton";
 
 const categoryNames = [
-  "Editor's Pick",
-  "Frontend Development",
-  "Product Design",
-  "Academic & Education",
-  "Biography",
-  "Children & Youth",
-  "Fiction & Literature",
-  "Religion",
-  "Lifestyle",
-  "Politics & Laws",
-  "Science & Research",
-  "Economics",
-  "Food",
-  "Marketing",
-  "Parenting",
-  "Relationships",
-  "Backend Development",
-  "Project Management",
-  "Arts & Design",
-  "Business & Career",
-  "Environment",
-  "Health & Fitness",
-  "Personal Growth",
-  "Tech",
-  "Entertainment",
-  "History",
-  "Motivation",
-  "Psychology",
-  "Sports",
+    "Editor's Pick",
+    "Frontend Development",
+    "Product Design",
+    "Academic & Education",
+    "Biography",
+    "Children & Youth",
+    "Fiction & Literature",
+    "Religion",
+    "Lifestyle",
+    "Politics & Laws",
+    "Science & Research",
+    "Economics",
+    "Food",
+    "Marketing",
+    "Parenting",
+    "Relationships",
+    "Backend Development",
+    "Project Management",
+    "Arts & Design",
+    "Business & Career",
+    "Environment",
+    "Health & Fitness",
+    "Personal Growth",
+    "Tech",
+    "Entertainment",
+    "History",
+    "Motivation",
+    "Psychology",
+    "Sports",
 ];
 
 const BookCategories = () => {
-  return (
-    <React.Fragment>
-      <body className="p-16">
-        <header>
-          <div>
-            <h1 className="font-semibold text-4xl">Book Categories</h1>
-          </div>
-          <div className="flex justify-between mt-2">
-            <p className="text-[#5B5B5B]">Select your favorite Categories</p>
-            <Link to="/">
-              {" "}
-              <p className="text-[#5B5B5B]">Switch to RoadMap</p>
-            </Link>
-          </div>
-        </header>
-        <main>
-          <div className="my-12 flex flex-wrap gap-6">
-            {categoryNames.map((category, index) => (
-              <CategoriesButton key={index} initialText={category} />
-            ))}
-          </div>
-        </main>
-        <footer className="flex justify-between">
-          <div className="w-2/5">
-            <Button borderVariant="noRadius" variant="primary">
-              Continue
-            </Button>{" "}
-          </div>
-          <p>Skip</p>
-        </footer>
-      </body>
-    </React.Fragment>
-  );
+    return (
+        <React.Fragment>
+            <body className="md:p-16 p-2">
+                <header>
+                    <div>
+                        <h1 className="font-semibold text-xl md:text-4xl">Book Categories</h1>
+                    </div>
+                    <div className="flex justify-between mt-2">
+                        <p className="text-[#5B5B5B] text-sm md:text-base">Select your favorite Categories</p>
+                        <Link to="/">
+                            {" "}
+                            <p className="text-[#5B5B5B] text-sm md:text-base">Switch to RoadMap</p>
+                        </Link>
+                    </div>
+                </header>
+                <main>
+                    <div className="my-12 flex flex-wrap gap-6">
+                        {categoryNames.map((category, index) => (
+                            <CategoriesButton key={index} initialText={category} />
+                        ))}
+                    </div>
+                </main>
+                <footer className="flex justify-between items-center ">
+                    <div className="w-1/2 md:w-2/5">
+                        <Button borderVariant="noRadius" variant="primary">
+                            Continue
+                        </Button>{" "}
+                    </div>
+                    <p className="text-[#5B5B5B]">Skip</p>
+                </footer>
+            </body>
+        </React.Fragment>
+    );
 };
 
 export default BookCategories;
