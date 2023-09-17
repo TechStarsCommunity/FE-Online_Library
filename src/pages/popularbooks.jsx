c020bbf5ea4f7214282d4762c3ec831527e4f138
blessingsmart <bublessingsmart@gmail.com>
2023-09-17 06:13:20 +0100

    modified:   src/pages/popularbooks.jsx

--- src/pages/popularbooks.jsx
+++ src/pages/popularbooks.jsx
@@ -4,8 +4,6 @@ import { BsSearch } from "react-icons/bs";
 import "../styles/home.css";
 import Button from "../components/button";
 
-
-
 const Popularbooks = () => {
     return (
         <>
@@ -15,28 +13,23 @@ const Popularbooks = () => {
                 </div>
                 <div className="flex flex-col m-10 ">
                     <div className="search-container">
-                            <BsSearch className="search-icon" />
-                            <input
-                                type="text"
-                                placeholder="Search"
-                            />
+                        <BsSearch className="search-icon" />
+                        <input type="text" placeholder="Search" />
                     </div>
                     <div>
                         <p>Categories</p>
                         <div></div>
                     </div>
                     <Button
-                            borderVariant="noRadius"
-                            variant="primary"
-                            className=" w-52 text-sm h-10"
-                        >
-                            Show all categories
+                        borderVariant="noRadius"
+                        variant="primary"
+                        className=" w-52 text-sm h-10"
+                    >
+                        Show all categories
                     </Button>
                     <div>
                         <p>Popular books</p>
-                        <div>
-                            
-                        </div>
+                        <div></div>
                     </div>
                 </div>
             </div>
