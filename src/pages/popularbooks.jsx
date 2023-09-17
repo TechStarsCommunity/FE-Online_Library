e8c815fb2a81312d2d1803960610b5f61f812f71
blessingsmart <bublessingsmart@gmail.com>
2023-09-17 19:51:43 +0100

    modified:   src/pages/popularbooks.jsx

--- src/pages/popularbooks.jsx
+++ src/pages/popularbooks.jsx
@@ -7,30 +7,35 @@ import Button from "../components/button";
 const Popularbooks = () => {
     return (
         <>
-            <div className="flex bg-slate-50 h-full  w-screen">
+            <div className="flex h-full  w-screen">
                 <div className="hidden sm:block">
                     <LeftSidebar />
                 </div>
-                <div className="flex flex-col m-10 ">
+                <div className="flex flex-col m-10 bg-black">
                     <div className="search-container">
                         <BsSearch className="search-icon" />
                         <input type="text" placeholder="Search" />
                     </div>
                     <div>
                         <p>Categories</p>
-                        <div className="p-2 bg-white rounded-lg flex">
-                            <div>
+                        <div className="p-5 bg-white rounded-lg h-32 flex">
+                            <div className="flex">
+                                <img src="./frontend.png" alt="Frontend Development" />
                                 <p>Frontend Development</p>
                             </div>
-                            <div>
+                            <div className="flex">
+                                <img src="./webdevelopment.png" alt="backend development" />
                                 <p>Backend Development</p>
                             </div>
-                            <div>
+                            <div className="flex">
+                                <img src="./designer.png" alt=" ui/ux designer" />
                                 <p>Product Design</p>
                             </div>
-                            <div>
+                            <div className="flex">
+                                <img src="./creativewriter.png" alt="content writer" />
                                 <p>Content Writing</p>
                             </div>
+                            <img src="./datascientist.png" alt="data scienctist" />
                         </div>
                     </div>
                     <Button
