b79876a0383e8a7fd3b2fa26bf2e029d0021cd75
hajora <sales@hajorah.com>
2023-09-19 16:26:06 +0100

    removed link router

--- src/pages/bookCategories.jsx
+++ src/pages/bookCategories.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Button from "../components/button";
-import { Link } from "react-router-dom";
 import CategoriesButton from "../components/categoriesButton";
 import { categoryNames } from "../data/categoryNames";
 
