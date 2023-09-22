da4f5227f21fb52156623bd128ca31cad1d0562d
salamolatunde <greenhousebiz24@gmail.com>
2023-09-22 12:57:46 +0100

    css file name

--- src/pages/carrierRoadmap.jsx
+++ src/pages/carrierRoadmap.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "../styles/frontendRoadmap.css";
+import "../styles/carrierRoadmap.css";
 import carrierImg1 from "../../public/carrierImg1.png";
 import carrierImg2 from "../../public/carrierImg2.png";
 import carrierImg3 from "../../public/carrierImg3.png";
