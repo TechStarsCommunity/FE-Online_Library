478ba10f90fddb9a4fec7988530874b1c24847f8
shakiroh55 <fhatshakirat@gmail.com>
2023-09-27 14:16:56 +0100

    worked on pagination

--- src/components/searchCard.jsx
+++ src/components/searchCard.jsx
@@ -3,21 +3,21 @@ import "../styles/searchCard.css";
 import Button from "../components/button";
 import { TechCareer } from "../data/search"
 import ReactPaginate from 'react-paginate';
-import { BsArrowLeftShort, BsArrowRightShort} from 'react-icons/bs'
+import { BsArrowLeftShort, BsArrowRightShort } from 'react-icons/bs'
 
 
 const SearchCard = () => {
     const [currentPage, setCurrentPage] = useState(0)
-    const perPage = 1;
+    const perPage = 5;
 
     const handlePageClick = (selectedPage) => {
         setCurrentPage(selectedPage.selected);
     };
-    
+
     const offset = currentPage * perPage;
     const pageCount = Math.ceil(TechCareer.length / perPage);
     const currentPageData = TechCareer.slice(offset, offset + perPage);
-    
+
     return (
         <React.Fragment>
             <div className="flex items-center justify-between">
@@ -26,15 +26,6 @@ const SearchCard = () => {
                 </div>
                 <div className="btn-search">
                     <Button className="btn-search">search</Button>{" "}
-                </div>
-                <div className="flex items-center justify-between w-full">
-                    <h3>97 results found in 0.33 seconds</h3>
-                    <select name="Sort By" id="" placeholder="Sort By">
-                        <option value="">Sort By</option>
-                        <option value="Latest">Latest</option>
-                        <option value="Old To New">Old To New</option>
-                        <option value="Techniclity">Techniclity</option>
-                    </select>
                 </div>
             </div>
             <div className="flex items-center justify-between resultFound">
@@ -80,20 +71,20 @@ const SearchCard = () => {
                     );
                 })}
 
-                <ReactPaginate
-                    previousLabel={<BsArrowLeftShort />}
-                    nextLabel={<BsArrowRightShort />}
-                    breakLabel={'...'}
-                    pageCount={pageCount}
-                    marginPagesDisplayed={2}
-                    pageRangeDisplayed={5}
-                    onPageChange={handlePageClick}
-                    containerClassName={`flex justify-center items-center h-[3rem] mt-[4rem] gap-[1rem] mb-[7rem]`}
-                    previousClassName={`p-2 bg-[#6F6F6F]`}
-                    nextClassName={`p-2 bg-[#6F6F6F]`}
-                    pageClassName={`bg-[#fff] px-2`}
-                    activeClassName={'bg-[#616161]'}
-                />
+            <ReactPaginate
+                previousLabel={<BsArrowLeftShort />}
+                nextLabel={<BsArrowRightShort />}
+                breakLabel={'...'}
+                pageCount={pageCount}
+                marginPagesDisplayed={2}
+                pageRangeDisplayed={5}
+                onPageChange={handlePageClick}
+                containerClassName={`flex justify-center items-center h-[3rem] mt-[4rem] gap-[1rem] mb-[7rem]`}
+                previousClassName={`p-2 bg-[#6F6F6F]`}
+                nextClassName={`p-2 bg-[#6F6F6F]`}
+                pageClassName={`bg-[#fff] px-2`}
+                activeClassName={'bg-[#616161]'}
+            />
         </React.Fragment>
     );
 };
