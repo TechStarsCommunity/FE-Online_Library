30637038f11a4ca0f9666c4d98a21693242ef18e
salamolatunde <greenhousebiz24@gmail.com>
2023-10-01 00:34:43 +0100

    more changes in the jsx and css files

--- src/pages/carrierRoadmap.jsx
+++ src/pages/carrierRoadmap.jsx
@@ -4,7 +4,7 @@ import carrierImg1 from "../../public/carrierImg1.png";
 import carrierImg2 from "../../public/carrierImg2.png";
 import carrierImg3 from "../../public/carrierImg3.png";
 import carrierImg4 from "../../public/carrierImg4.png";
-import icon from "../../public/frontendicon.svg";
+import icon from "../../public/iconfrontend.svg";
 import arrow1 from "../../public/arrowRight.svg";
 import arrow2 from "../../public/arrowCurve.svg";
 import arrow3 from "../../public/arrowDown.svg";
@@ -12,8 +12,8 @@ import arrow3 from "../../public/arrowDown.svg";
 const Carrierroadmap = () => {
     return (
         <div className="FrontMap-container">
-            <h1>Roadmap</h1>
-            <h3>Frontend Roadmap</h3>
+            <h1 className="title1">Roadmap</h1>
+            <h3 className="title2">Frontend Roadmap</h3>
 
             <div className="FrontImg-container">
                 <div className="Roadmap-container">
@@ -23,11 +23,13 @@ const Carrierroadmap = () => {
 
                     <input type="Text" placeholder="Oluwaben Akeem" className="Name" />
 
-                    <img src={icon} alt="Iconimg" className="Icon"/>
-                    <p className="Frontend-text">Frontend Development</p>
+                    <div className="input">
+                        <img src={icon} alt="Iconimg" className="Icon" />
+                        <input type="Text" placeholder="Frontend Development" className="Name" /> 
                     </div>
+                </div>
 
-                    <img src={arrow1} alt="ArrowImg" className="Path1" />
+                <img src={arrow1} alt="ArrowImg" className="Path1" />
 
                 <div className="Roadmap-container">
                     <img src={carrierImg2} alt="Image" className="FrontImg" />
@@ -36,8 +38,10 @@ const Carrierroadmap = () => {
 
                     <input type="Text" placeholder="Oluwaben Akeem" className="Name" />
 
-                    <img src={icon} alt="Iconimg" className="Icon"/>
-                    <p className="Frontend-text">Frontend Development</p>
+                    <div className="input">
+                        <img src={icon} alt="Iconimg" className="Icon" />
+                        <input type="Text" placeholder="Frontend Development" className="Name" /> 
+                    </div>
                 </div>
 
                 <img src={arrow1} alt="ArrowImg" className="Path1" />
@@ -47,9 +51,12 @@ const Carrierroadmap = () => {
 
                     <p className="Carrier-text">Starting a Successful Carrier in Tech</p>
 
-                    <input type="Text" placeholder="Oluwaben Akeem" className="Name" />
+                    <input type="Text" placeholder="Oluwaben Akeem" className="Name" /> 
 
-                    <input type="Text" placeholder="Frontend Development" className="Name" />
+                    <div className="input">
+                        <img src={icon} alt="Iconimg" className="Icon" />
+                        <input type="Text" placeholder="Frontend Development" className="Name" /> 
+                    </div>
                 </div>
 
                 <img src={arrow1} alt="ArrowImg" className="Path1" />
@@ -61,10 +68,12 @@ const Carrierroadmap = () => {
 
                     <input type="Text" placeholder="Oluwaben Akeem" className="Name" />
 
-                    <img src={icon} alt="Iconimg" className="Icon"/>
-                    <p className="Frontend-text">Frontend Development</p>
-
+                    <div className="input">
+                        <img src={icon} alt="Iconimg" className="Icon" />
+                        <input type="Text" placeholder="Frontend Development" className="Name" /> 
+                    </div>
                 </div>
+            
 
                 <img src={arrow2} alt="ArrowImg" className="Path2" />
 
@@ -75,9 +84,10 @@ const Carrierroadmap = () => {
 
                     <input type="Text" placeholder="Project" className="Name" />
 
-                    <img src={icon} alt="Iconimg" className="Icon"/>
-                    <p className="Frontend-text">HTML Test</p>
-
+                    <div className="input">
+                        <img src={icon} alt="Iconimg" className="Icon" />
+                        <input type="Text" placeholder="HTML" className="Name" /> 
+                    </div>
                 </div>
 
                 <img src={arrow3} alt="ArrowImg" className="Path3" />
