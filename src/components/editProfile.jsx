a56d1ff1bf9886bd20eb3a8b75d7c25b14d61aab
cEbuka <kastecebuka@gmail.com>
2023-10-08 20:32:08 +0100

    late tweaks

--- src/components/editProfile.jsx
+++ src/components/editProfile.jsx
@@ -11,7 +11,7 @@ const editProfile = () => {
     const { errors, register, handleSubmit } = useSubmit(EditProfileSchema);
 
     const processData = (data) => {
-        console.log(data);
+        console.log(data)
     };
 
     return (
