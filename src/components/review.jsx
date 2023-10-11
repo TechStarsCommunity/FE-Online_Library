db53c52439ff5266e6134a2a6cc90718b7a9b117
canhamzacode <abdulmuizzayo6@gmail.com>
2023-10-11 23:11:21 +0200

    fixed the review card

--- src/components/review.jsx
+++ src/components/review.jsx
@@ -4,53 +4,57 @@ import ReviewCard from "./reviewCard";
 import { myData } from "../data/reviewData";
 
 const Review = () => {
-  const [currentIndex, setCurrentIndex] = useState(0);
+    const [currentIndex, setCurrentIndex] = useState(0);
 
-  const increaseIndex = () => {
-    if (currentIndex + 2 < myData.length) {
-      setCurrentIndex(currentIndex + 2);
-    }
-  };
+    const increaseIndex = () => {
+        if (currentIndex + 2 < myData.length) {
+            setCurrentIndex(currentIndex + 2);
+        }
+    };
 
-  const decreaseIndex = () => {
-    if (currentIndex > 0) {
-      setCurrentIndex(currentIndex - 2);
-    }
-  };
+    const decreaseIndex = () => {
+        if (currentIndex > 0) {
+            setCurrentIndex(currentIndex - 2);
+        }
+    };
 
-  return (
-    <div className="w-full p-[40px] bg-[#F3FEFB] relative grid gap-[10px]">
-      <h1 className="text-[24px] md:text-[30px] my-[20px] text-[#203A66] font-bold lg:text-[36px]">
-        What our BooksLab readers have to say
-      </h1>
-      <div className="w-full grid md:grid-cols-2 gap-[25px] grid-cols-1 mb-[20px]">
-        {myData.slice(currentIndex, currentIndex + 2).map((data, index) => (
-          <ReviewCard
-            key={index}
-            comment={data.comment}
-            userName={data.name}
-            stack={data.stack}
-          />
-        ))}
-      </div>
-      <div className="flex items-center gap-[25px] absolute right-[20px] bottom-0 p-[10px]">
-        <button
-          className={`bg-[#000] rounded-[50%] p-[5px] text-[#fff] ${currentIndex === 0 ? "bg-[grey]" : ""}`}
-          onClick={decreaseIndex}
-          disabled={currentIndex === 0}
-        >
-          <AiOutlineLeft size={25} />
-        </button>
-        <button
-          className={`bg-[#000] rounded-[50%] p-[5px] text-[#fff] ${currentIndex + 2 >= myData.length ? "bg-[grey]" : ""}`}
-          onClick={increaseIndex}
-          disabled={currentIndex + 2 >= myData.length}
-        >
-          <AiOutlineRight size={25} />
-        </button>
-      </div>
-    </div>
-  );
+    return (
+        <div className="w-full p-[40px] bg-[#F3FEFB] relative grid gap-[10px]">
+            <h1 className="text-[24px] md:text-[30px] my-[20px] text-[#203A66] font-bold lg:text-[36px]">
+                What our BooksLab readers have to say
+            </h1>
+            <div className="w-full grid md:grid-cols-2 gap-[25px] grid-cols-1 mb-[20px]">
+                {myData.slice(currentIndex, currentIndex + 2).map((data, index) => (
+                    <ReviewCard
+                        key={index}
+                        comment={data.comment}
+                        userName={data.name}
+                        stack={data.stack}
+                    />
+                ))}
+            </div>
+            <div className="flex items-center gap-[25px] absolute right-[20px] bottom-0 p-[10px]">
+                <button
+                    className={`bg-[#000] rounded-[50%] p-[5px] text-[#fff] ${
+                        currentIndex === 0 ? "bg-[grey]" : ""
+                    }`}
+                    onClick={decreaseIndex}
+                    disabled={currentIndex === 0}
+                >
+                    <AiOutlineLeft size={25} />
+                </button>
+                <button
+                    className={`bg-[#000] rounded-[50%] p-[5px] text-[#fff] ${
+                        currentIndex + 2 >= myData.length ? "bg-[grey]" : ""
+                    }`}
+                    onClick={increaseIndex}
+                    disabled={currentIndex + 2 >= myData.length}
+                >
+                    <AiOutlineRight size={25} />
+                </button>
+            </div>
+        </div>
+    );
 };
 
 export default Review;
--- src/components/reviewCard.jsx
+++ src/components/reviewCard.jsx
@@ -1,30 +1,30 @@
 import React from "react";
 
 const ReviewCard = ({ comment, userName, stack }) => {
-  return (
-    <div className="w-full grid gap-[25px] p-[20px] rounded-md bg-[#fff] relative">
-      <div className="flex items-center gap-[15px] rounded-md w-full">
-        <div className="w-[50px] h-[50px] rounded-[50%] bg-[grey]">
-          <img
-            src="http://localhost:5174/contact_us_illustration.png"
-            alt=""
-            className="w-full h-full"
-          />
+    return (
+        <div className="w-full grid gap-[25px] p-[20px] rounded-md bg-[#fff] relative">
+            <div className="flex items-center gap-[15px] rounded-md w-full">
+                <div className="w-[50px] h-[50px] rounded-[50%] bg-[grey]">
+                    <img
+                        src="http://localhost:5174/contact_us_illustration.png"
+                        alt=""
+                        className="w-full h-full"
+                    />
+                </div>
+                <div className="w-full">
+                    <p className="text-base font-bold">{userName}</p>
+                    <p className="text-base line-clamp-2">{stack} Developer</p>
+                </div>
+            </div>
+            <div className="w-full">
+                <p className="text-lg line-clamp-3">{comment}</p>
+            </div>
+            <div className="flex justify-end flex-col items-end self-end">
+                <p className="text-sm">10/12/2023</p>
+                <p className="text-sm">4:40pm</p>
+            </div>
         </div>
-        <div className="w-full">
-          <p className="text-base font-bold">{userName}</p>
-          <p className="text-base line-clamp-2">{stack} Developer</p>
-        </div>
-      </div>
-      <div className="w-full">
-        <p className="text-lg line-clamp-3">{comment}</p>
-      </div>
-      <div className="flex justify-end flex-col items-end self-end">
-        <p className="text-sm">10/12/2023</p>
-        <p className="text-sm">4:40pm</p>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default ReviewCard;
