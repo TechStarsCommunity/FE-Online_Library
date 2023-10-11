fc33380ba3388237cde8758bd458c9bb1a20c9e3
canhamzacode <abdulmuizzayo6@gmail.com>
2023-10-11 22:28:21 +0200

    fixed the review card

--- src/components/review.jsx
+++ src/components/review.jsx
@@ -4,50 +4,53 @@ import ReviewCard from "./reviewCard";
 import { myData } from "../data/reviewData";
 
 const Review = () => {
-    const [activeData, setActiveData] = useState([myData[0], myData[1]]);
-    const [currentIndex, setCurrentIndex] = useState(1);
+  const [currentIndex, setCurrentIndex] = useState(0);
 
-    const increaseIndex = () => {
-        if (currentIndex + 2 <= myData.length) {
-            setCurrentIndex(currentIndex + 2);
-        }
-    };
+  const increaseIndex = () => {
+    if (currentIndex + 2 < myData.length) {
+      setCurrentIndex(currentIndex + 2);
+    }
+  };
 
-    const decreaseIndex = () => {
-        if (currentIndex > 1) {
-            setCurrentIndex(currentIndex - 2);
-        }
-    };
+  const decreaseIndex = () => {
+    if (currentIndex > 0) {
+      setCurrentIndex(currentIndex - 2);
+    }
+  };
 
-    useEffect(() => {
-        setActiveData([myData[currentIndex - 1], myData[currentIndex]]);
-    }, [currentIndex]);
-
-    return (
-        <div className="w-full p-[40px] bg-[#F3FEFB] relative grid gap-[10px]">
-            <h1 className="text-[24px] md:text-[30px] my-[20px] text-[#203A66] font-bold lg:text-[36px]">
-                What our BooksLab readers have to say
-            </h1>
-            <div className="w-full grid md:grid-cols-2 gap-[25px] grid-cols-1 mb-[20px]">
-                {activeData.map((data, index) => (
-                    <ReviewCard
-                        key={index}
-                        comment={data.comment}
-                        userName={data.name}
-                        stack={data.stack}
-                    />
-                ))}
-            </div>
-            <div className="flex items-center gap-[25px] absolute right-[20px] bottom-0 p-[10px]">
-                <div className="bg-[#000] rounded-[50%] p-[5px] text-[#fff]">
-                    <AiOutlineLeft size={25} onClick={increaseIndex} />
-                </div>
-                <div className="bg-[#000] rounded-[50%] p-[5px] text-[#fff]">
-                    <AiOutlineRight size={25} onClick={decreaseIndex} />
-                </div>
-            </div>
-        </div>
-    );
+  return (
+    <div className="w-full p-[40px] bg-[#F3FEFB] relative grid gap-[10px]">
+      <h1 className="text-[24px] md:text-[30px] my-[20px] text-[#203A66] font-bold lg:text-[36px]">
+        What our BooksLab readers have to say
+      </h1>
+      <div className="w-full grid md:grid-cols-2 gap-[25px] grid-cols-1 mb-[20px]">
+        {myData.slice(currentIndex, currentIndex + 2).map((data, index) => (
+          <ReviewCard
+            key={index}
+            comment={data.comment}
+            userName={data.name}
+            stack={data.stack}
+          />
+        ))}
+      </div>
+      <div className="flex items-center gap-[25px] absolute right-[20px] bottom-0 p-[10px]">
+        <button
+          className={`bg-[#000] rounded-[50%] p-[5px] text-[#fff] ${currentIndex === 0 ? "bg-[grey]" : ""}`}
+          onClick={decreaseIndex}
+          disabled={currentIndex === 0}
+        >
+          <AiOutlineLeft size={25} />
+        </button>
+        <button
+          className={`bg-[#000] rounded-[50%] p-[5px] text-[#fff] ${currentIndex + 2 >= myData.length ? "bg-[grey]" : ""}`}
+          onClick={increaseIndex}
+          disabled={currentIndex + 2 >= myData.length}
+        >
+          <AiOutlineRight size={25} />
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Review;
--- src/data/reviewData.js
+++ src/data/reviewData.js
@@ -3,24 +3,24 @@ export const myData = [
         name: "Amanda",
         stack: "Frontend",
         comment:
-            "wjsglsgm anknk flg snlsgn wjsglsgm anknk flg snl wjsglsgm anknk flg snlsgn wjsglsgm anknk flg snlsgn wjsglsgm anknk flg snlsgn wjsglsgm anknk flg snlsgn",
+            "Choose BooksLab as your go-to platform for books and resources, and let us help you achieve your learning goals. Join us on a knowledge-driven adventure and thrive in the ever-evolving tech world.",
     },
     {
         name: "Kelechi",
         stack: "Backend",
         comment:
-            "wjsglsgm anknk flg snlsgn wjsglsgm anknk flg snl wjsglsgm anknk flg snlsgn wjsglsgm anknk flg snlsgn wjsglsgm anknk flg snlsgn wjsglsgm anknk flg snlsgn",
+            "Welcome to BooksLab, your online resource library for books and articles. Created by the Techstars community, we connect individuals worldwide with a vast collection of verified books curated by experts. O",
     },
     {
         name: "Product",
         stack: "Product",
         comment:
-            "wjsglsgm anknk flg snlsgn wjsglsgm anknk flg snl wjsglsgm anknk flg snlsgn wjsglsgm anknk flg snlsgn wjsglsgm anknk flg snlsgn wjsglsgm anknk flg snlsgn",
+            " Our unique roadmap feature guides you from beginner to advanced in your chosen tech field, with regular updates to keep you up-to-date on the latest trends and technologies.",
     },
     {
         name: "Hakeem",
         stack: "Backend",
         comment:
-            "wjsglsgm anknk flg snlsgn wjsglsgm anknk flg snl wjsglsgm anknk flg snlsgn wjsglsgm anknk flg snlsgn wjsglsgm anknk flg snlsgn wjsglsgm anknk flg snlsgn",
+            "At BooksLab, we believe knowledge should be accessible to all. That's why our platform is completely free and features user-generated resources that undergo thorough verification by our expert community. ",
     },
 ];
