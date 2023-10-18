a40be3c7fddfb19edd3e2ddb3a625db32971c221
shakiroh55 <fhatshakirat@gmail.com>
2023-10-18 09:30:12 +0100

    making the sideBar collapsable

--- src/pages/uploads.jsx
+++ src/pages/uploads.jsx
@@ -4,6 +4,16 @@ import LeftSidebar from "../components/leftSideBar";
 import { Tabs } from "../data/leftSideBarData";
 import { IoIosMenu } from "react-icons/io";
 
+// new
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+// import Dashboard from './pages/Dashboard.jsx';
+import BookCategories from "./bookCategories";
+import YourLibrary from "../components/yourLibrary";
+import RoadMap from "../components/roadmap";
+import UploadBooks from "./UploadBooks";
+import Profile from "./Profile";
+
+
 const Uploads = () => {
     const [active, setActive] = useState(1);
     const [sidebarVisible, setSidebarVisible] = useState(true);
@@ -15,37 +25,40 @@ const Uploads = () => {
     };
 
     return (
-        <React.Fragment>
-            <div className="flex">
-                <button
-                    className="transition-transform duration-300 transform hover:scale-110"
-                    onClick={toogleSidebar}
-                >
-                    <IoIosMenu
-                        style={{
-                            fontSize: "40px",
-                            color: "gray",
-                            marginTop: "40px",
-                            marginLeft: "20px",
-                        }}
-                    />
-                </button>
-
-                {sidebarVisible && (
-                    <div
-                        className={`bg-[#DFF6FF] h-full px-0 py-4 fixed mx-0 transition-all duration-300 ease-in-out ${
-                            sidebarVisible ? "left-0" : "-left-[20.6rem]"
-                        }`}
+        <>
+            <React.Fragment>
+                <div className="flex">
+                    <button
+                        className="transition-transform duration-300 transform hover:scale-110"
+                        onClick={toogleSidebar}
                     >
-                        <LeftSidebar active={active} setActive={setActive} />
+                        <IoIosMenu
+                            style={{
+                                fontSize: "40px",
+                                color: "gray",
+                                marginTop: "40px",
+                                marginLeft: "20px",
+                            }}
+                        />
+                    </button>
+                    {sidebarVisible && (
+                        <div
+                            className={`bg-[#DFF6FF] h-full px-0 py-4 fixed mx-0 transition-all duration-300 ease-in-out ${
+                                sidebarVisible ? "left-0" : "-left-[20.6rem]"
+                            }`}
+                        >
+                            <LeftSidebar active={active} setActive={setActive} />
+                        </div>
+                    )}
+                    <div className="flex-grow absolute right-0 min-h-100vh left-[20.6rem] pl-10">
+                        {activeTabInfo && <activeTabInfo.component />}
                     </div>
-                )}
-
-                <div className="flex-grow absolute right-0 min-h-100vh left-[20.6rem] pl-10">
-                    {activeTabInfo && <activeTabInfo.component />}
                 </div>
-            </div>
-        </React.Fragment>
+            </React.Fragment>
+
+            
+     
+        </>
     );
 };
 export default Uploads;
