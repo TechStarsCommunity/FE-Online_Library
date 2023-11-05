72259b27b201cb4da9c8c85af94f24b3ee3c9efd
Enyia Christian <94331754+Christiano112@users.noreply.github.com>
2023-11-05 20:14:55 +0100

    updates

--- src/data/reviewData.js
+++ src/data/reviewData.js
@@ -3,42 +3,42 @@ export const myData = [
         name: "Amanda",
         stack: "Frontend",
         comment:
-            "wjsglsgm anknk flg snlsgn wjsglsgm anknk flg snl wjsglsgm anknk flg snlsgn wjsglsgm anknk flg snlsgn wjsglsgm anknk flg snlsgn wjsglsgm anknk flg snlsgn",
+            "Choose BooksLab as your go-to platform for books and resources, and let us help you achieve your learning goals. Join us on a knowledge-driven adventure and thrive in the ever-evolving tech world.",
     },
     {
         name: "Kelechi",
         stack: "Backend",
         comment:
-            "wjsglsgm anknk flg snlsgn wjsglsgm anknk flg snl wjsglsgm anknk flg snlsgn wjsglsgm anknk flg snlsgn wjsglsgm anknk flg snlsgn wjsglsgm anknk flg snlsgn",
+            "Welcome to BooksLab, your online resource library for books and articles. Created by the Techstars community, we connect individuals worldwide with a vast collection of verified books curated by experts.",
     },
     {
         name: "Isaac",
         stack: "Frontend",
         comment:
-            "wjsglsgm anknk flg snlsgn wjsglsgm anknk flg snl wjsglsgm anknk flg snlsgn wjsglsgm anknk flg snlsgn wjsglsgm anknk flg snlsgn wjsglsgm anknk flg snlsgn",
+            "Our unique roadmap feature guides you from beginner to advanced in your chosen tech field, with regular updates to keep you up-to-date on the latest trends and technologies.",
     },
     {
         name: "Hakeem",
         stack: "Manager",
         comment:
-            "wjsglsgm anknk flg snlsgn wjsglsgm anknk flg snl wjsglsgm anknk flg snlsgn wjsglsgm anknk flg snlsgn wjsglsgm anknk flg snlsgn wjsglsgm anknk flg snlsgn",
+            "We are a community of tech enthusiasts, and we are passionate about helping you achieve your learning goals. Our platform is designed to help you learn and grow in your chosen tech field.",
     },
     {
         name: "Temitope",
         stack: "Frontend",
         comment:
-            "wjsglsgm anknk flg snlsgn wjsglsgm anknk flg snl wjsglsgm anknk flg snlsgn wjsglsgm anknk flg snlsgn wjsglsgm anknk flg snlsgn wjsglsgm anknk flg snlsgn",
+            "We are a community of tech enthusiasts, and we are passionate about helping you achieve your learning goals. Our platform is designed to help you learn and grow in your chosen tech field.",
     },
     {
         name: "Vince",
         stack: "Backend",
         comment:
-            "wjsglsgm anknk flg snlsgn wjsglsgm anknk flg snl wjsglsgm anknk flg snlsgn wjsglsgm anknk flg snlsgn wjsglsgm anknk flg snlsgn wjsglsgm anknk flg snlsgn",
+            "Our unique roadmap feature guides you from beginner to advanced in your chosen tech field, with regular updates to keep you up-to-date on the latest trends and technologies.",
     },
     {
         name: "Adtech",
         stack: "Product",
         comment:
-            "wjsglsgm anknk flg snlsgn wjsglsgm anknk flg snl wjsglsgm anknk flg snlsgn wjsglsgm anknk flg snlsgn wjsglsgm anknk flg snlsgn wjsglsgm anknk flg snlsgn",
+            "Welcome to BooksLab, your online resource library for books and articles. Created by the Techstars community, we connect individuals worldwide with a vast collection of verified books curated by experts.",
     },
 ];
