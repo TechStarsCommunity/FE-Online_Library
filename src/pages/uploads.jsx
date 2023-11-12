de79267643a6e730c9bd693f4933c92d534d9f98
Temitope Akinsunmade <temitopeakinsunmade08@gmail.com>
2023-11-12 19:36:35 +0100

    MADE the uploads page responsive

--- src/pages/uploads.jsx
+++ src/pages/uploads.jsx
@@ -5,86 +5,90 @@ import { Tabs } from "../data/leftSideBarData";
 import { MdMenu, MdClose } from "react-icons/md";
 
 const Uploads = () => {
-  const [active, setActive] = useState(1);
-  const [sidebarOpen, setSidebarOpen] = useState(false); // Default to closed on mobile
+    const [active, setActive] = useState(1);
+    const [sidebarOpen, setSidebarOpen] = useState(false); // Default to closed on mobile
 
-  const activeTabInfo = Tabs.find((tab) => tab.id === active);
+    const activeTabInfo = Tabs.find((tab) => tab.id === active);
 
-  const toggleSidebar = (event) => {
-    event.stopPropagation();
-    setSidebarOpen(!sidebarOpen);
-  };
-
-  useEffect(() => {
-    const handleResize = () => {
-      const isMobile = window.innerWidth < 898; 
-      setSidebarOpen(!isMobile); 
+    const toggleSidebar = (event) => {
+        event.stopPropagation();
+        setSidebarOpen(!sidebarOpen);
     };
 
-    handleResize();
+    useEffect(() => {
+        const handleResize = () => {
+            const isMobile = window.innerWidth < 898;
+            setSidebarOpen(!isMobile);
+        };
 
-    window.addEventListener("resize", handleResize);
+        handleResize();
 
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []); 
+        window.addEventListener("resize", handleResize);
 
-  return (
-    <React.Fragment>
-      <div className="flex relative">
-        <div className="flex">
-          <button
-            className="transition-transform duration-300 transform hover:scale-110 cursor-pointer absolute right-0 lg:hidden block"
-            onClick={toggleSidebar}
-          >
-            {sidebarOpen ? (
-              <MdClose
-                style={{
-                  fontSize: "40px",
-                  color: "gray",
-                  marginTop: "40px",
-                  marginLeft: "20px",
-                }}
-              />
-            ) : (
-              <MdMenu
-                style={{
-                  fontSize: "40px",
-                  color: "gray",
-                  marginTop: "40px",
-                  marginLeft: "20px",
-                }}
-              />
-            )}
-          </button>
+        return () => {
+            window.removeEventListener("resize", handleResize);
+        };
+    }, []);
 
-          {sidebarOpen && (
-            <div
-              className={`fixed top-0 left-0 w-screen h-screen bg-black opacity-${
-                sidebarOpen ? "50" : "0"
-              } transition-opacity duration-300 z-40`}
-              onClick={toggleSidebar}
-            ></div>
-          )}
-        </div>
+    return (
+        <React.Fragment>
+            <div className="flex relative">
+                <div className="flex">
+                    <button
+                        className="transition-transform duration-300 transform hover:scale-110 cursor-pointer absolute right-0 lg:hidden block"
+                        onClick={toggleSidebar}
+                    >
+                        {sidebarOpen ? (
+                            <MdClose
+                                style={{
+                                    fontSize: "40px",
+                                    color: "gray",
+                                    marginTop: "40px",
+                                    marginLeft: "20px",
+                                }}
+                            />
+                        ) : (
+                            <MdMenu
+                                style={{
+                                    fontSize: "40px",
+                                    color: "gray",
+                                    marginTop: "40px",
+                                    marginLeft: "20px",
+                                }}
+                            />
+                        )}
+                    </button>
 
-        <div className={`flex-grow min-h-100vh absolute right-0 ${sidebarOpen ? "left-[25%]" : "left-0"} ml-0`}>
-          {activeTabInfo && <activeTabInfo.component />}
-        </div>
+                    {sidebarOpen && (
+                        <div
+                            className={`fixed top-0 left-0 w-screen h-screen bg-black opacity-${
+                                sidebarOpen ? "50" : "0"
+                            } transition-opacity duration-300 z-40`}
+                            onClick={toggleSidebar}
+                        ></div>
+                    )}
+                </div>
 
-        {sidebarOpen && (
-          <div
-            className={`bg-[#DFF6FF] h-full px-0 py-4 fixed mx-0 transition-all duration-300 ease-in-out z-50 ${
-              sidebarOpen ? "top-0" : "-top-full"
-            }`}
-          >
-            <LeftSidebar active={active} setActive={setActive} />
-          </div>
-        )}
-      </div>
-    </React.Fragment>
-  );
+                <div
+                    className={`flex-grow min-h-100vh absolute right-0 ${
+                        sidebarOpen ? "left-[25%]" : "left-0"
+                    } ml-0`}
+                >
+                    {activeTabInfo && <activeTabInfo.component />}
+                </div>
+
+                {sidebarOpen && (
+                    <div
+                        className={`bg-[#DFF6FF] h-full px-0 py-4 fixed mx-0 transition-all duration-300 ease-in-out z-50 ${
+                            sidebarOpen ? "top-0" : "-top-full"
+                        }`}
+                    >
+                        <LeftSidebar active={active} setActive={setActive} />
+                    </div>
+                )}
+            </div>
+        </React.Fragment>
+    );
 };
 
 export default Uploads;
